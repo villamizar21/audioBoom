eadd203dc1b73746b1167bd8e03385e8fd9092f0
villamizar21 <90472621+villamizar21@users.noreply.github.com>
2022-06-23 21:14:51 +0200

    Ajuste -se ajusta la reproducción del audio

--- app/src/main/java/com/example/audioboom/mainModule/view/fragments/PlayHighFragment.kt
+++ app/src/main/java/com/example/audioboom/mainModule/view/fragments/PlayHighFragment.kt
@@ -1,26 +1,25 @@
 package com.example.audioboom.mainModule.view.fragments
 
+import android.media.AudioManager
+import android.media.MediaPlayer
 import android.os.Bundle
-import android.util.Log
+import android.os.Handler
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.SeekBar
+import androidx.core.net.toUri
 import com.bumptech.glide.Glide
 import com.example.audioboom.databinding.FragmentPlayHighBinding
-import com.google.android.exoplayer2.ExoPlayer
-import com.google.android.exoplayer2.MediaItem
-import com.google.android.exoplayer2.Player
-import com.google.android.exoplayer2.SimpleExoPlayer
-import com.google.android.exoplayer2.ui.PlayerControlView
 
 
-class PlayHighFragment : Fragment(), Player.Listener {
-
-    private lateinit var player: ExoPlayer
-    private lateinit var playerView: PlayerControlView
+class PlayHighFragment : Fragment() {
 
     private lateinit var binding: FragmentPlayHighBinding
+    private lateinit var runnable: Runnable
+    private lateinit var handler: Handler
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -32,31 +31,59 @@ class PlayHighFragment : Fragment(), Player.Listener {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        sutupPlayer()
+
         getData()
     }
 
-    private fun sutupPlayer() {
-        player = ExoPlayer.Builder(requireContext()).build()
-        playerView = binding.player
-        playerView.player = player
-        player.addListener(this)
+    private fun sutupPlayer(song: String) {
+        val mediaPlayer = MediaPlayer()
+        mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC)
+        mediaPlayer.setDataSource(song)
+        binding.seekBar.progress = 0
+        binding.seekBar.max = mediaPlayer.duration
+
+        binding.playMedia.setOnClickListener {
+            if (!mediaPlayer.isPlaying)
+                mediaPlayer.start()
+            else
+                mediaPlayer.stop()
+
+        }
+
+        binding.seekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(p0: SeekBar?, pos: Int, changed: Boolean) {
+                if (changed)
+                    mediaPlayer.seekTo(pos)
+            }
+
+            override fun onStartTrackingTouch(p0: SeekBar?) {
+            }
+
+            override fun onStopTrackingTouch(p0: SeekBar?) {
+            }
+
+        })
+        runnable = Runnable {
+            binding.seekBar.progress = mediaPlayer.currentPosition
+            handler.postDelayed(runnable, 1000)
+        }
+        handler.postDelayed(runnable, 1000)
+
+        mediaPlayer.setOnCompletionListener {
+            binding.seekBar.progress = 0
+        }
     }
+
 
     private fun getData() {
         val song = arguments?.getString("song") ?: ""
         val title = arguments?.getString("title") ?: ""
         val image = arguments?.getString("image") ?: ""
-        val item: MediaItem = MediaItem.fromUri(song)
-        initPlayList(item)
+        sutupPlayer(song)
         Glide.with(this)
             .load(image)
             .into(binding.photoPlay)
     }
 
-    private fun initPlayList(item: MediaItem) {
-        player.addMediaItem(item)
-        player.prepare()
 
-    }
 }
